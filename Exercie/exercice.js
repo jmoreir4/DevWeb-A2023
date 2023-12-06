4b2745c5700693c89e1c33682af6bae379216842
Josony <jmoreir4@users.noreply.github.com>
2023-12-06 09:36:28 +0100

    Create exercice.js

---
+++ Exercie/exercice.js
@@ -0,0 +1,8 @@
+let element = document.querySelectorAll('#button').addEventListener("click",function(){
+  console.log
+})
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
